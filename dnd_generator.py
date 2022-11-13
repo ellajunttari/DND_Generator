b9357d2f260dd51731fc7f6aeb75b231c1923120
elmajunt <elmajunt@student.jyu.fi>
2022-11-13 20:57:28 +0200

    added name generation

--- dnd_generator.py
+++ dnd_generator.py
@@ -7,6 +7,9 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def randomize_race():
@@ -26,39 +29,39 @@ def randomize_class():
     sub_class = random.choice(sub_classes)
     return {"Class" : main_class, "Subclass" : sub_class}
 
-def give_name(character_class):
-    #link = f"https://www.fantasynamegenerators.com/dnd-{character_class}-names.php"
-
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get("https://www.google.com")
-    #page = urlopen(link)
-    #html_bytes = page.read()
-    #html = html_bytes.decode("utf-8")
-    # sending get request and saving the response as response object
-    #r = requests.get(url = link)
-    # extracting data in json format
-    #response_data = r.json()
-    #print("data: " + response_data)
-    #print(link)
-    return "testi"
+def give_name(character_race):
+    link = f"https://www.fantasynamegenerators.com/dnd-{character_race.lower()}-names.php"
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.get(link)
+    try:
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='ncmp__banner-btns']/button[2]"))).click()
+    except:
+        pass
+    try:
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//input[contains(@value, 'Female names')]"))).click()
+    except:
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//input[contains(@value, 'Get female names')]"))).click()
+    names = driver.find_element(By.ID, "result")
+    name = names.text.split('\n')[0]
+    driver.close()
+    return name
 
 def print_information(character):
+    print(f"Your name is {character['Name']}")
     if character["Race"][0] in ['A','E','I','O','U','Y']:
         print(f"You are an {character['Race']} {character['Class']}.")
     else:
         print(f"You are a {character['Race']} {character['Class']}.")
-    print(f"Your subclass is {character['Subclass']}")
+    print(f"Your subclass is {character['Subclass']}.")
 
 f = open('dnd_data.json')
 data = json.load(f)
 character = {}
 character["Race"] = randomize_race()
 character.update(randomize_class())
-character["Name"] = give_name(character["Class"])
+character["Name"] = give_name(character["Race"])
 print_information(character)
-# create webdriver object
-driver = webdriver.Firefox()
-# get google.co.in
-driver.get(f"https://www.fantasynamegenerators.com/dnd-{character_class}-names.php")
 
 f.close()
