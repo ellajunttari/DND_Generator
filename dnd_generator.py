dfbac215cacb7cc964e03ba3e3b7b879ba2c8be2
elmajunt <elmajunt@student.jyu.fi>
2023-12-02 00:18:54 +0200

    thinking about moving this to flash, we'll see

--- dnd_generator.py
+++ dnd_generator.py
@@ -4,14 +4,8 @@ import json
 import random
 from urllib.request import urlopen
 import requests
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 
-api_root = "https://www.dnd5eapi.co/api/"
+API_ROOT = "https://www.dnd5eapi.co/api/"
 
 def generate_character(character):
     character["Level"] = get_level()
@@ -25,12 +19,12 @@ def get_level():
     return val
 
 def get_race():
-    race = random.choice(requests.get(f"{api_root}races").json()["results"])
+    race = random.choice(requests.get(f"{API_ROOT}races").json()["results"])
     subrace = get_sub_race(race)
     return [race, subrace]
 
 def get_sub_race(race):
-    subraces = requests.get(f"{api_root}races/{race['index']}/subraces").json()
+    subraces = requests.get(f"{API_ROOT}races/{race['index']}/subraces").json()
     if subraces["count"] != 0:
         subrace = random.choice(subraces['results'])
     else:
@@ -38,17 +32,17 @@ def get_sub_race(race):
     return subrace
 
 def get_main_class():
-    dnd_class = random.choice(requests.get(f"{api_root}classes").json()["results"])
-    subclass = get_subclass(dnd_class)
+    dnd_class = random.choice(requests.get(f"{API_ROOT}classes").json()["results"])
+    subclass = "" if int(character["Level"]) < 3 else get_subclass(dnd_class)
     return [dnd_class, subclass]
 
 def get_subclass(dnd_class):
-    subclass = random.choice(requests.get(f"{api_root}classes/{dnd_class['index']}/subclasses").json()["results"])
+    subclass = random.choice(requests.get(f"{API_ROOT}classes/{dnd_class['index']}/subclasses").json()["results"])
     return subclass
 
 def give_name():
-    chartopia_api_root = "https://chartopia.d12dev.com/api"
-    name_results = requests.post(f"{chartopia_api_root}/charts/69548/roll/").json()['results'][0]
+    chartopia_API_ROOT = "https://chartopia.d12dev.com/api"
+    name_results = requests.post(f"{chartopia_API_ROOT}/charts/69548/roll/").json()['results'][0]
     name = name_results.replace("\n ", "")
     name = name.replace(" ", "", 1)
     return name
@@ -73,12 +67,14 @@ def roll_a_score():
     return score
 
 def print_character(character):
-    print(f"Your name is {character['Name']}")
+    print(f"\nYour name is {character['Name']}")
     if character["Race"]['name'] in ['A','E','I','O','U','Y']:
-        print(f"You are an {character['Race']['name']} {character['Class']['name']}. Your subclass is {character['Subclass']['name']}.")
+        print(f"You are an {character['Race']['name']} {character['Class']['name']}.")
     else:
-        print(f"You are a {character['Race']['name']} {character['Class']['name']}. Your subclass is {character['Subclass']['name']}.")
-        print("Your ability scores are: ")
+        print(f"You are a {character['Race']['name']} {character['Class']['name']}.")
+    if character["Subclass"]:
+        print(f"Your subclass is {character['Subclass']['name']}.")
+    print("Your ability scores are: ")
     for score in character["Scores"]:
         print(f"{score}: {character['Scores'][score]}")
 
